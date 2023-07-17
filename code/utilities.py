604854267c970bd4a9d487a7582bc69813676423
Fadi Hamad <fadihamad1994@gmail.com>
2023-07-17 14:19:47 -0400

    Update code to save more data Compute statistics for datasets Compute AUC score for the individual untuned models

--- code/utilities.py
+++ code/utilities.py
@@ -98,8 +98,35 @@ def load_data(data_set_name:str):
         raise ValueError("Invalid data set name: " + data_set_name)
     return df_original, target
 
+def load_data_original(data_set_name:str, balanced:bool=False):
+    adult_data_set_dir = "../data/adult"
+    credit_card_data_set_dir = "../data/credit_card"
+    adult_data_set_csv_file_name = 'adult'
+    balaned_credit_card_data_set_csv_file_name = 'credit_card_balanced'
+    unbalaned_credit_card_data_set_csv_file_name = 'credit_card_unbalanced'
+    df_original = pd.DataFrame()
+    if data_set_name == 'adult':
+        df_original = pd.read_csv(adult_data_set_dir + '/' + adult_data_set_csv_file_name + '.csv')
+        target = 'income'
+        df_original.loc[df_original[target] == "<=50K", target] = 0
+        df_original.loc[df_original[target] == ">50K", target] = 1
+        df_original.replace('?', np.NaN,inplace=True)
+        df_original.dropna(axis=0,how='any',inplace=True)
+    elif data_set_name == 'credit_card':
+        if balanced:
+            df_original = pd.read_csv(credit_card_data_set_dir + '/' + balaned_credit_card_data_set_csv_file_name + '.csv')
+        else:
+            df_original = pd.read_csv(credit_card_data_set_dir + '/' + unbalaned_credit_card_data_set_csv_file_name + '.csv')
+    else:
+        raise ValueError("Invalid data set name: " + data_set_name)
+    return df_original
 
 def save_test_train_data(data_set_name, df_train, df_test, balanced:bool=False):
+    adult_data_set_dir = "../data/adult"
+    credit_card_data_set_dir = "../data/credit_card"
+    adult_data_set_csv_file_name = 'adult'
+    balaned_credit_card_data_set_csv_file_name = 'credit_card_balanced'
+    unbalaned_credit_card_data_set_csv_file_name = 'credit_card_unbalanced'
     if data_set_name == 'adult':
         df_train.to_csv(adult_data_set_dir + '/' + adult_data_set_csv_file_name + '_train.csv', index=False)
         df_test.to_csv(adult_data_set_dir + '/' + adult_data_set_csv_file_name + '_testcsv', index=False)
