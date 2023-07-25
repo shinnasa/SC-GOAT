6caa259f82c65a2000168de2dd1e4a8f700168d7
Shinpei Nakamura <nash@ciencias.unam.mx>
2023-07-24 20:43:04 -0400

    optim

--- scripts/optimization_approach.sh
+++ scripts/optimization_approach.sh
@@ -1,31 +1,41 @@
+#!/bin/sh
+#SBATCH --partition gpu
+#SBATCH --time 24:00:00
+#SBATCH --cpus-per-task 12
+#SBATCH --gpus 1
+#SBATCH --mem-per-cpu 3G
+#SBATCH --mail-user=s.nakamura.sakai@yale.edu
+#SBATCH --mail-type=ALL
+module load miniconda
+conda activate sdv
 #encode balanced tuned
 # Untuned
 # Without Target Encoder
 # Adult Data Set
 # Data Num_iter Encode Balanced Tuned
-python3 code/OptimizationApproach_XGBoost.py adult 2000 False False False
+python code/OptimizationApproach_XGBoost.py adult 20000 False False False
 
 # # Unbalanced Credit Card Data Set
-python3 code/OptimizationApproach_XGBoost.py credit_card 2000 False False False
+python code/OptimizationApproach_XGBoost.py credit_card 20000 False False False
 
 # # Balanced Credit Card Data Set
-python3 code/OptimizationApproach_XGBoost.py credit_card 2000 False True False
+python code/OptimizationApproach_XGBoost.py credit_card 20000 False True False
 
 # With Target Encoder
 # Adult Data Set
-python3 code/OptimizationApproach_XGBoost.py adult 2000 True False False
+python code/OptimizationApproach_XGBoost.py adult 20000 True False False
 
 # Tuned
 # Without Target Encoder
 # Adult Data Set
-python3 code/OptimizationApproach_XGBoost.py adult 2000 False False True
+python code/OptimizationApproach_XGBoost.py adult 20000 False False True
 
 # # Unbalanced Credit Card Data Set (need files)
-python3 code/OptimizationApproach_XGBoost.py credit_card 2000 False False True
+python code/OptimizationApproach_XGBoost.py credit_card 100 False False True
 
 # # Balanced Credit Card Data Set
-python3 code/OptimizationApproach_XGBoost.py credit_card 2000 False True True
+python code/OptimizationApproach_XGBoost.py credit_card 20000 False True True
 
 # With Target Encoder 
 # Adult Data Set (need files)
-python3 code/OptimizationApproach_XGBoost.py adult 2000 True False True
+python code/OptimizationApproach_XGBoost.py adult 20000 True False True
